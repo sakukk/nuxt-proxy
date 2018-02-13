ff41ff2aabc93f179d84a881f2d8199cfbd13616
Yyyyyyyk <403549324@qq.com>
2018-02-13 18:35:06 +0800

    修改代理

--- pages/douban.vue
+++ pages/douban.vue
@@ -8,7 +8,8 @@
   export default {
 		name: 'douban',
     async asyncData ({ app }) {
-      let {data} = await app.$axios.get('https://api.douban.com/v2/book/1220562')
+      // let {data} = await app.$axios.get('https://api.douban.com/v2/book/1220562')
+      let {data} = await app.$axios.get('/api/v2/book/1220562')
       return { test : data }
     },
     data () {
