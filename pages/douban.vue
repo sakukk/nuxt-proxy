919112a5f7cc51fca3c890a2f0fe47e34ce5c02e
Yyyyyyyk <403549324@qq.com>
2018-02-13 18:32:57 +0800

    commit

---
+++ pages/douban.vue
@@ -0,0 +1,24 @@
+<template>
+	<section>
+    <p>{{test}}</p>
+  </section>
+</template>
+
+<script>
+  export default {
+		name: 'douban',
+    async asyncData ({ app }) {
+      let {data} = await app.$axios.get('https://api.douban.com/v2/book/1220562')
+      return { test : data }
+    },
+    data () {
+      return {
+        test : ''
+      }
+    }
+	}
+</script>
+
+<style scoped>
+
+</style>
